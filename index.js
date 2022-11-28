ea9bf7fd565f7dc04e92cd32db6b95e18fd4a8f1
Charles Swaleh <charlswall@live.com>
2022-11-28 17:47:04 +0300

    Completed strings assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+
+const currentUser = "Charles";
+
+const welcomeMessage = `Welcome to Flatbook, +${currentUser}!`
+
+const excitedWelcomeMessage = `Welcome to Flatbook, ${currentUser}!`.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
